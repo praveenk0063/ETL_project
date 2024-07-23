1aa3ca8f2b12ed81b87deee9818682be8169b8e1
Praveen Kumar <38518979+praveenk0063@users.noreply.github.com>
2024-07-23 20:26:22 +0530

    Add files via upload

---
+++ config_db.py
@@ -0,0 +1,7 @@
+DATABASE_CONFIG = {
+    'user': 'username',  #your username
+    'password': 'password', #your database password
+    'host': 'localhost', #your host name
+    'database': 'db_name', #Database name
+    'port': '5432' #Database port number
+}
